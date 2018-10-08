424a2222bd9429bd99494b55f4149cc96680c9b7
Eugene Paitoo <36279649+epaitoo@users.noreply.github.com>
2018-10-08 22:35:32 +0000

    Changed the numberPerPages to 10

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 const studentList = Array.from(document.querySelectorAll('.student-item')); //Selects student list
 // Random number I chose to have students show per page.
-const numberPerPage = 3;
+const numberPerPage = 10;
 
 // The querySelectorAll get a node list, so I used the Array.from to make it into an array.
 // .forEach just does a for loop, but without all the extra stuff.
-Array.from(document.querySelectorAll('div li a'))
-  .forEach(page => page.addEventListener('click', (e) => {
-    // I added the event listener to all the links so when they are clicked this funciton runs.
-    showPage(e.srcElement.textContent, studentList);
-  }));
+// Array.from(document.querySelectorAll('div li a'))
+//   .forEach(page => page.addEventListener('click', (e) => {
+//     // I added the event listener to all the links so when they are clicked this funciton runs.
+//     showPage(e.srcElement.textContent, studentList);
+//   }));
 
 function showPage(pageNum, studentList) {
   studentList.forEach((student, i) => {
@@ -18,9 +18,9 @@ function showPage(pageNum, studentList) {
 
     // Check that the index we are looping over is one that we care about.
     if (start <= i && i <= end) {
-      studentList[i].style.display = 'block'; //hide these students in the list
+      studentList[i].style.display = 'block'; //displays these students in the list
     } else {
-      studentList[i].style.display = 'none'; //display these students in the list
+      studentList[i].style.display = 'none'; //hide these students in the list
     }
   });
 }

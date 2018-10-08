7bdf6a3d83a5a68770018a69507a40d8cb93a266
Eugene Paitoo <eugenepaitoo@gmail.com>
2018-10-08 15:11:59 +0000

    Worked on the showpage function

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ let pageNum = Math.ceil(studentList.length / 10); //returns 6
 function showPage(pageNum, studentList) {
     listUl.style.display = 'none'; //hide students in the list
     for (let i = 0; i < studentList.length; i++) {
-        if (pageNum <= 6) { 
-         listUl.style.display = 'block'; //display students in the list
-        }
-    }
-}
+        if (studentList[i] > 10) { 
+            studentList[i].style.display = 'none'; //hide students in the list
+           }  else {
+               studentList[i].style.display = 'block'; //display students in the list
+           }
+       }
+   }
 
 

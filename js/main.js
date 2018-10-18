58b003141e5eaad4ff88f9a57290bab771566481
Eugene Paitoo <eugenepaitoo@gmail.com>
2018-10-18 15:18:01 +0000

    updated the showpage function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 const listUl = document.querySelector('.student-list'); //Selects Ul element containing the studentList
 const studentList = document.getElementsByClassName('student-item cf'); //Selects student list
-let pageNum = Math.ceil(studentList.length / 10); //returns 6
+const page = document.querySelector('.page'); //selects the whole page
 
-function showPage(pageNum, studentList) {
-    listUl.style.display = 'none'; //hide students in the list
-    for (let i = 0; i < studentList.length; i++) {
-        if (studentList[i] > 10) { 
-            studentList[i].style.display = 'none'; //hide students in the list
-           }  else {
-               studentList[i].style.display = 'block'; //display students in the list
-           }
-       }
+
+function showpage(pageNum, studentList) {
+   for (let i = 0; i < studentList.length; i++) {
+    // Get the first index that should display and the last index to display
+    const firstItem = pageNum * numberPerPage - numberPerPage;
+    const lastItem = pageNum * numberPerPage - 1;
+    if (i >= firstItem && i <= lastItem) {
+      studentList[i].style.display = 'block';
+    } else {
+      studentList[i].style.display = 'none';
+    }
    }
-
+}
 

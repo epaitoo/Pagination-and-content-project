7a3170e39cebfc3833acdc6f7918d2f0629db7ac
Eugene Paitoo <eugenepaitoo@gmail.com>
2018-10-18 17:27:25 +0000

    Added an appendPageLinks function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-const listUl = document.querySelector('.student-list'); //Selects Ul element containing the studentList
-const studentList = document.getElementsByClassName('student-item cf'); //Selects student list
+const studentList = Array.from(document.querySelectorAll('.student-item')); //selects all the students
+const numberPerPage = 10;
 const page = document.querySelector('.page'); //selects the whole page
 
 
-function showpage(pageNum, studentList) {
+function showPage(pageNum, studentList) {
    for (let i = 0; i < studentList.length; i++) {
     // Get the first index that should display and the last index to display
     const firstItem = pageNum * numberPerPage - numberPerPage;
@@ -16,3 +16,55 @@ function showpage(pageNum, studentList) {
    }
 }
 
+// This function creates all the page links based on a list of students
+function appendPageLinks(studentList) {
+  // determine how many pages for this student list
+  let pages = Math.ceil(studentList.length / numberPerPage); //returns 6
+
+  //creating pagination
+  const paginationDiv = document.createElement('div'); 
+  paginationDiv.className = 'pagination';
+  page.appendChild(paginationDiv);
+
+  const pageUl = document.createElement('ul'); //create ul 
+  paginationDiv.appendChild(pageUl);
+  
+  // create a page link section and page link to the page link section
+  for (let i = 1; i <= pages; i++) {
+    const pageList = document.createElement('li');
+    const pageLink = document.createElement('a');
+    pageLink.textContent = i;
+    pageLink.setAttribute('href', '#');
+    pageUl.appendChild(pageList);
+    pageList.appendChild(pageLink);
+  }  
+
+  const paginationLinks = document.querySelectorAll('.pagination a'); 
+
+  paginationLinks[0].className = 'active'; 
+  
+  for (let i =0; i < paginationLinks.length; i++) {
+    paginationLinks[i].addEventListener('click', () => {
+      // remove active class from all the links
+		for (let i=0; i < paginationLinks.length; i++) {
+			paginationLinks[i].className='';
+		}
+		
+		// store the clicked number in a variable named clickedPageNumber
+		let clickedPageNumber = paginationLinks[i].textContent;
+		
+		// use the showPage function to display the page for the link clicked
+		showPage(clickedPageNumber, studentList);
+		
+		// mark that link as “active”
+paginationLinks[i].className = 'active';
+
+    });
+  }
+}
+
+
+
+showPage(1, studentList); //Display the first 10 students on load
+
+appendPageLinks(studentList);
